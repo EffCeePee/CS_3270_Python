bfcd93a35736c565e85900bf91cfeb5294e3ecf8
Colton <coltonparry@gmail.com>
2015-01-15 12:40:37 -0700

    P1

---
+++ p1.py
@@ -0,0 +1,22 @@
+﻿
+territoriesFile = open('territories.txt')
+gameStateFile = open('gameState.txt')
+tline = territoriesFile.readline()
+sline = gameStateFile.readline()
+# temp = []
+
+territoriesDict = {}
+
+while tline:
+	temp = tline.split(',')
+	print(temp)
+	tline = territoriesFile.readline()
+territoriesFile.close()
+
+
+while sline:
+	temp = sline.split(',')
+	print(temp)
+	sline = gameStateFile.readline()
+gameStateFile.close()
+
